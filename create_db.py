d1348b2de751b8ad878aace52bb82bbc2a96e458
Oleg <nehnaev@gmail.com>
2023-05-09 19:01:05 +0300

    database creation

--- create_db.py
+++ create_db.py
@@ -17,19 +17,20 @@
 # conn.close()
 import sqlite3
 
-conn = sqlite3.connect("mydata.db")
+conn = sqlite3.connect("mydata1.db")
 
-sql = "CREATE TABLE siriust (email TEXT, first_name TEXT, last_name TEXT, city TEXT)"
-# sql = "CREATE TABLE favorites (title TEXT, retail_price INT, wholesale_price INT, all_ratings INT, count_shop INT, text TEXT, star_reviews INT)"
-sql = "SELECT * FROM siriust"
+sql_s = "CREATE TABLE siriust (email TEXT, first_name TEXT, last_name TEXT, city TEXT)"
+sql_f = "CREATE TABLE favorites (title TEXT, retail_price INT, wholesale_price INT, all_ratings INT, count_shop INT, text TEXT, star_reviews INT)"
+# sql = "SELECT * FROM siriust"
 
 cursor = conn.cursor()
 
-cursor.execute(sql)
+cursor.execute(sql_s)
+cursor.execute(sql_f)
 
 res = cursor.fetchall()
 # #
-for r in res:
-    print(r)
+# for r in res:
+#     print(r)
 
 conn.close()
