89b5e7fd5e87a2941ca7746012b8e83e4784ae18
Ken760 <49761163+Ken760@users.noreply.github.com>
2023-05-09 21:24:28 +0300

    update

--- create_db.py
+++ create_db.py
@@ -1,27 +1,9 @@
-# import sqlite3
-#
-# conn = sqlite3.connect("mydata.db")
-#
-# sql = "CREATE TABLE siriust (email TEXT, first_name TEXT, last_name TEXT, city TEXT)"
-# # sql = "SELECT * FROM siriust"
-#
-# cursor = conn.cursor()
-#
-# cursor.execute(sql)
-#
-# res = cursor.fetchall()
-# #
-# # for r in res:
-# #     print(r)
-#
-# conn.close()
 import sqlite3
 
-conn = sqlite3.connect("mydata1.db")
+conn = sqlite3.connect("mydata.db")
 
 sql_s = "CREATE TABLE siriust (email TEXT, first_name TEXT, last_name TEXT, city TEXT)"
-sql_f = "CREATE TABLE favorites (title TEXT, retail_price INT, wholesale_price INT, all_ratings INT, count_shop INT, text TEXT, star_reviews INT)"
-# sql = "SELECT * FROM siriust"
+sql_f = "CREATE TABLE favorites (title TEXT, retail_price INT, wholesale_price INT, all_ratings INT, count_shop INT, reviews_text TEXT, star_reviews INT)"
 
 cursor = conn.cursor()
 
@@ -29,8 +11,5 @@ cursor.execute(sql_s)
 cursor.execute(sql_f)
 
 res = cursor.fetchall()
-# #
-# for r in res:
-#     print(r)
 
 conn.close()
