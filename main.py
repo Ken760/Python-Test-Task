c944e9c763e7ccf59df4bee9caa17296272b29e4
Ken760 <49761163+Ken760@users.noreply.github.com>
2023-05-10 14:02:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import requests
-from bs4 import BeautifulSoup
+from lxml import html
 import sqlite3
 
 user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36 Edg/112.0.1722.68'
@@ -14,8 +14,8 @@ password = input('Введите пароль: ')
 data = {
     "return_url": "index.php",
     "redirect_url": "index.php",
-    "user_login": login,
-    "password": password,
+    "user_login": 'nehnaev@gmail.com',
+    "password": 'ThooVur4dave',
     "dispatch[auth.login]": ""
 }
 
@@ -36,14 +36,12 @@ def get_profile_page():
     """Функция получения данных из профиля"""
     get_auth()
     profile_page = s.get('https://siriust.ru/profiles-update/', headers=header)
-    soup = BeautifulSoup(profile_page.text, "lxml")
-    email = soup.find_all('input', class_="ty-input-text cm-focus")[0]['value']
-    first_name = soup.find_all('input', class_="ty-input-text cm-focus")[1]['value']
-    last_name = soup.find_all('input', class_="ty-input-text")[12]['value']
-
-    city = soup.find_all('input', class_="ty-input-text")[15]['value']
-    profile_info.append((email, first_name, last_name, city))
-    return profile_info
+    profile = html.fromstring(profile_page.text)
+    email = profile.xpath('//*[@id="email"]/@value')
+    first_name = profile.xpath('//*[@id="elm_15"]/@value')
+    last_name = profile.xpath('//*[@id="elm_17"]/@value')
+    city = profile.xpath('//*[@id="elm_23"]/@value')
+    profile_info.append((*email, *first_name, *last_name, *city))
 
 
 def get_favourite_page():
@@ -51,49 +49,44 @@ def get_favourite_page():
     get_auth()
     favorites = s.get('https://siriust.ru/wishlist/', headers=header)
     favorite_links = []
-    favorite = BeautifulSoup(favorites.text, "lxml")
-    for link in favorite.find_all('a', class_='product-title'):
-        favorite_links.append(link['href'])
-
+    favorite = html.fromstring(favorites.text)
+    for link in favorite.xpath('//a[@class="product-title"]/@href'):
+        favorite_links.append(link)
     for links in favorite_links:
-        count = 0
+        available_counter = 0
         all_reviews = []
         all_ratings = ''
-        reviews_text = ''
         all_favorite_links = s.get(links)
-        soup = BeautifulSoup(all_favorite_links.text, 'lxml')
-        title = soup.find('h1', class_='ty-product-block-title').text
-        retail_price = soup.find_all('span', class_='ty-price-num')[0].text.strip().replace(u'\xa0', u' ')
-        wholesale_price = soup.find_all('span', class_='ty-price-num')[2].text.strip().replace(u'\xa0', u' ')
-        rating = soup.find_all('div', class_='ty-product-block__rating')
+        favourite = html.fromstring(all_favorite_links.text, 'lxml')
+        title = favourite.xpath('//*[@id="tygh_main_container"]/div[3]/div/div[1]/div/div/div[1]/div/h1/bdi')[0].text.strip()
+        retail_price = favourite.cssselect('span.ty-price-num')[0].text.strip().replace(u'\xa0', u' ')
+        wholesale_price = favourite.cssselect('span.ty-price-num')[2].text.strip().replace(u'\xa0', u' ')
+        rating = favourite.xpath('//*[@id="tygh_main_container"]/div[3]/div/div[1]/div/div/div[1]/div/div[3]/div/div[1]/div[1]')
         for star in rating:
-            ratings = star.find_all('i', class_='ty-stars__icon ty-icon-star')
+            ratings = star.xpath('//*[@id="average_rating_product"]/span/a/i')
             all_ratings = len(ratings)
-        shops = soup.find_all('div', class_='ty-product-feature__value')
-        for shop in shops:
-            available = shop.text.strip().replace('—  ', '')
-            if available != 'отсутствует' and available != '':
-                count += 1
-
-        reviews = soup.find_all('div', class_='ty-discussion-post__message')
+        reviews = favourite.cssselect('div.ty-discussion-post__message')
         for review in reviews:
             all_reviews.append(review.text)
-        for text_reviews in all_reviews:
-            reviews_text = all_reviews
-        number_of_reviews = soup.find('a', 'ty-discussion__review-a cm-external-click')
-        if number_of_reviews is None:
+        number_of_reviews = favourite.xpath('//*[@id="average_rating_product"]/a[1]')
+        if not number_of_reviews:
             star_reviews = 0
         else:
-            star_reviews = int(number_of_reviews.text[0])
+            star_reviews = int(number_of_reviews[0].text[0])
+        shops = favourite.xpath('//*[@id="content_features"]/div/div/div/text()')
+        for shop in shops:
+            available = shop.strip().replace('—  ', '')
+            if available != 'отсутствует' and available != '':
+                available_counter += 1
         favourite_list.append(
-            (title, retail_price, wholesale_price, all_ratings, count, str(reviews_text), star_reviews))
+            (title, retail_price, wholesale_price, all_ratings, available_counter, str(all_reviews), star_reviews))
 
 
 def add_db():
     """Функция добавления данных в базу"""
     get_profile_page()
     get_favourite_page()
-    conn = sqlite3.connect("mydata1.db")
+    conn = sqlite3.connect("mydata.db")
     cursor = conn.cursor()
 
     cursor.executemany("INSERT INTO profile VALUES (?, ?, ?, ?)", profile_info)
