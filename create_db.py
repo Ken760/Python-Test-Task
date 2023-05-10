e10df90ad83d3669d1ff745844fd41ae5003fcb0
Ken760 <49761163+Ken760@users.noreply.github.com>
2023-05-10 14:02:48 +0300

    Update create_db.py

--- create_db.py
+++ create_db.py
@@ -3,7 +3,7 @@ import sqlite3
 conn = sqlite3.connect("mydata.db")
 
 sql_profile = "CREATE TABLE profile (email TEXT, first_name TEXT, last_name TEXT, city TEXT)"
-sql_favorites = "CREATE TABLE favorites (title TEXT, retail_price INT, wholesale_price INT, all_ratings INT, count_shop INT, reviews_text TEXT, star_reviews INT)"
+sql_favorites = "CREATE TABLE favorites (title TEXT, retail_price INT, wholesale_price INT, all_ratings INT, availability_counter INT, reviews_text TEXT, star_reviews INT)"
 
 cursor = conn.cursor()
 
